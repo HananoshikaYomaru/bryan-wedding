591c7bfd6ff806213b2bab107c516595a8b68c11
Yomaru <mlkyeung@connect.ust.hk>
2022-01-02 17:23:18 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      height : { 
+        "144" : "36rem" , 
+        "200" : "50rem" , 
+        "240" : "60rem" , 
+        "300" : "75rem" , 
+        "400" : "100rem" , 
+      }, 
+      colors: {
+        "pickled-bluewood": "#253948",
+        "grain-brown": "#E1C8B2",
+        "soapstone": "#FFFBF8",
+        "granny-smith": "#899E9F",
+        "sage": "#9DA086",
+        "wheat": "#F3DAB3",
+        "brandy": "#DFC89E",
+      },
+      fontFamily: {
+        // ...defaultTheme.fontFamily ,
+        "thankyou": ["thank you"],
+        "wenyue": ["wenyue"],
+        "copperplate": ["copperplate"],
+        "amibata" : ["amibata"] ,
+        "firefly" : ["firefly"] ,
+      }
+    },
+  },
+  plugins: [],
+}
