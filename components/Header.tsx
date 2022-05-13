d5e2824d8b403da4c80e9f83b54dc3b480c67d95
Hananoshika Yomaru <brenkysbwim@gmail.com>
2022-05-13 18:45:27 +0800

    change date

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,57 +1,94 @@
 import { useLocale } from "../lib/locale";
-import cover from "../public/images/cover.webp"
-import Image from "next/image"
+import cover from "../public/images/cover.webp";
+import Image from "next/image";
 import Link from "next/link";
 import { MenuAlt2Icon } from "@heroicons/react/solid";
 import { Popover } from "@headlessui/react";
 import { useMediaQuery } from "../lib/mediaQuery";
 
-
 const Header = () => {
-  const { getLocale, changeLocale , L } = useLocale()
-  const isTabletOrMobile = useMediaQuery('(max-width: 1224px)')
+  const { getLocale, changeLocale, L } = useLocale();
+  const isTabletOrMobile = useMediaQuery("(max-width: 1224px)");
 
-
-
-  return <div className="relative w-full h-20">
-    <Image src={cover} layout="fill" objectFit="cover" className="pointer-events-none" draggable={false}></Image>
-    {
-      !isTabletOrMobile &&
-      <div>
-        <div className="absolute flex flex-row space-x-16 center">
-          <Link href={`/${getLocale()}`}><a className="bartext" >{L().layout.home}</a></Link>
-          <Link href={`/${getLocale()}/info`}><a className="bartext">{L().layout.information}</a></Link>
-          {/* <Link href={`/${getLocale()}/events`}><a className="bartext">{L().layout.events}</a></Link> */}
-          {/* <Link href={`/${getLocale()}/story`}><a className="bartext" >{L().layout.story}</a></Link> */}
-          <Link href={`/${getLocale()}/gallery`}><a className="bartext" >{L().layout.gallery}</a></Link>
+  return (
+    <div className="relative w-full h-20">
+      <Image
+        src={cover}
+        layout="fill"
+        objectFit="cover"
+        className="pointer-events-none"
+        draggable={false}
+      ></Image>
+      {!isTabletOrMobile && (
+        <div>
+          <div className="absolute flex flex-row space-x-16 center">
+            <Link href={`/${getLocale()}`}>
+              <a className="bartext">{L().layout.home}</a>
+            </Link>
+            <Link href={`/${getLocale()}/info`}>
+              <a className="bartext">{L().layout.information}</a>
+            </Link>
+            {/* <Link href={`/${getLocale()}/events`}>
+              <a className="bartext">{L().layout.events}</a>
+            </Link> */}
+            {/* <Link href={`/${getLocale()}/story`}><a className="bartext" >{L().layout.story}</a></Link> */}
+            <Link href={`/${getLocale()}/gallery`}>
+              <a className="bartext">{L().layout.gallery}</a>
+            </Link>
+          </div>
         </div>
-      </div>
-
-    }
-    {
-      isTabletOrMobile && <div>
-        <Popover className="relative mt-6  ml-5 z-20">
-          <Popover.Button><MenuAlt2Icon className="absolute w-8 h-8 text-grain-brown z-20 top-0 left-0"></MenuAlt2Icon></Popover.Button>
-          <Popover.Panel className="absolute z-10 bg-stone-300 p-10 rounded-xl shadow mt-5">
-            {
-              ({ close }) => {
+      )}
+      {isTabletOrMobile && (
+        <div>
+          <Popover className="relative mt-6  ml-5 z-20">
+            <Popover.Button>
+              <MenuAlt2Icon className="absolute w-8 h-8 text-grain-brown z-20 top-0 left-0"></MenuAlt2Icon>
+            </Popover.Button>
+            <Popover.Panel className="absolute z-10 bg-stone-300 p-10 rounded-xl shadow mt-5">
+              {({ close }) => {
                 return (
                   <div className="flex flex-col space-y-5">
-                    <Link href={`/${getLocale()}`}><a className="font-copperplate text-pickled-bluewood hover:bg-slate-200 whitespace-nowrap" onClick={()=>close()}>{L().layout.home}</a></Link>
-                    <Link href={`/${getLocale()}/info`}><a className="font-copperplate text-pickled-bluewood hover:bg-slate-200  whitespace-nowrap" onClick={()=>close()}>{L().layout.information}</a></Link>
+                    <Link href={`/${getLocale()}`}>
+                      <a
+                        className="font-copperplate text-pickled-bluewood hover:bg-slate-200 whitespace-nowrap"
+                        onClick={() => close()}
+                      >
+                        {L().layout.home}
+                      </a>
+                    </Link>
+                    <Link href={`/${getLocale()}/info`}>
+                      <a
+                        className="font-copperplate text-pickled-bluewood hover:bg-slate-200  whitespace-nowrap"
+                        onClick={() => close()}
+                      >
+                        {L().layout.information}
+                      </a>
+                    </Link>
                     {/* <Link href={`/${getLocale()}/events`}><a className="font-copperplate text-pickled-bluewood hover:bg-slate-200 whitespace-nowrap" onClick={()=>close()}>{L().layout.events}</a></Link> */}
                     {/* <Link href={`/${getLocale()}/story`}><a className="font-copperplate text-pickled-bluewood hover:bg-slate-200 whitespace-nowrap"  onClick={()=>close()}>{L().layout.story}</a></Link> */}
-                    <Link href={`/${getLocale()}/gallery`}><a className="font-copperplate text-pickled-bluewood hover:bg-slate-200 whitespace-nowrap"  onClick={()=>close()}>{L().layout.gallery}</a></Link>
+                    <Link href={`/${getLocale()}/gallery`}>
+                      <a
+                        className="font-copperplate text-pickled-bluewood hover:bg-slate-200 whitespace-nowrap"
+                        onClick={() => close()}
+                      >
+                        {L().layout.gallery}
+                      </a>
+                    </Link>
                   </div>
-                )
-              }
-            }
-          </Popover.Panel>
-        </Popover>
-      </div>
-    }
-    {/* <button className="font-wenyue text-grain-brown absolute top-1/2 right-0  transform -translate-y-1/2 mr-5 sm:mr-12" onClick={() => changeLocale(getLocale() == "en" ? "zh-hk" : "en")}>{getLocale() == "en" ? " 🇭🇰 中文" : " 🇬🇧 English"}</button> */}
-  </div>
-}
+                );
+              }}
+            </Popover.Panel>
+          </Popover>
+        </div>
+      )}
+      <button
+        className="font-wenyue text-grain-brown absolute top-1/2 right-0  transform -translate-y-1/2 mr-5 sm:mr-12"
+        onClick={() => changeLocale(getLocale() == "en" ? "zh-hk" : "en")}
+      >
+        {getLocale() == "en" ? " 🇭🇰 中文" : " 🇬🇧 English"}
+      </button>
+    </div>
+  );
+};
 
-export default Header; 
+export default Header;
--- constants/localization.ts
+++ constants/localization.ts
@@ -1,276 +1,272 @@
 // the structure of location is wrong
 // the correct structure should each string has its own locale and provide a fallback locale instead has each locale has different strings
 
-import moment from "moment"
-import { title } from "process"
+import moment from "moment";
+import { title } from "process";
 
 type Event = {
-    title: string,
-    description: string,
-    date: string,
-}
+  title: string;
+  description: string;
+  date: string;
+};
 
 type Memory = {
-    title?: string,
-    description?: string,
-}
+  title?: string;
+  description?: string;
+};
 
 export type Data = {
-    layout: {
-        home: string,
-        information: string,
-        events: string,
-        story: string,
-        gallery: string,
-    },
-    home: {
-        wedding_day: string,
-        story: string,
-        info: string,
-        about_us: string,
-        des1: string,
-        des2: string,
-        day_until: string,
-        days: string,
-        hours: string,
-        minutes: string,
-        seconds: string,
-    },
-    info: {
-        location: string,
-        details: string,
-        gallery: string,
-        title: string,
-        // details 
-        date_time: string,
-        date_time_data: string,
-        location_data: string,
-        dress_code: string,
-        dress_code_data: string,
-        video_link: string,
-        // shuttle bus
-        shuttle_bus: string,
-        time: string,
-        pick_up_location: string,
-        pick_up_location_data: string,
-    }
-    events: {
-        title: string,
-        date: string,
-        events: Event[],
-    },
-    gallery: {
-        gallery: string,
-        sections: {
-            title: string,
-            memories: Memory[],
-        }[]
-    }
-}
+  layout: {
+    home: string;
+    information: string;
+    events: string;
+    story: string;
+    gallery: string;
+  };
+  home: {
+    wedding_day: string;
+    story: string;
+    info: string;
+    about_us: string;
+    des1: string;
+    des2: string;
+    day_until: string;
+    days: string;
+    hours: string;
+    minutes: string;
+    seconds: string;
+  };
+  info: {
+    location: string;
+    details: string;
+    gallery: string;
+    title: string;
+    // details
+    date_time: string;
+    date_time_data: string;
+    location_data: string;
+    dress_code: string;
+    dress_code_data: string;
+    video_link: string;
+    // shuttle bus
+    shuttle_bus: string;
+    time: string;
+    pick_up_location: string;
+    pick_up_location_data: string;
+  };
+  events: {
+    title: string;
+    date: string;
+    events: Event[];
+  };
+  gallery: {
+    gallery: string;
+    sections: {
+      title: string;
+      memories: Memory[];
+    }[];
+  };
+};
 
 export const L: { [locale: string]: Data } = {
-    "en": {
-        layout: {
-            home: "Home",
-            information: "Wedding Day Information",
-            events: "Upcoming Events",
-            story: "Our Story",
-            gallery: "Our Gallery",
-        },
-        home: {
-            wedding_day: "We are getting married!",
-            story: "Our Story",
-            info: "Information",
-            about_us: "About Us",
-            des1: "We met in church, a place blessed by the Lord, in 2013. Despite our characters being so different and opposite like sense and sensitivity, we draw to each other seeing how beautiful the heart and soul inside us that make the others become better and emerged in love. - Fanny",
-            des2: "After countless journeys and adventures, we finally decided to take the next step in our lives as husband and wife. What other way to celebrate this, than in the company of our beloved family and friends!",
-            day_until: "Date We Getting Married",
-            days: "Days",
-            hours: "Hours",
-            minutes: "Minutes",
-            seconds: "Seconds",
-        },
-        info: {
-            location: "Location",
-            details: "Details",
-            gallery: "Gallery",
-            title: "Wedding Day Information",
-            // details 
-            date_time: "Date & Time",
-            date_time_data: "5 March 2022, 13 : 30 - 16 : 30",
-            location_data: "Auberge Discovery Bay Hotel, Hong Kong",
-            dress_code: "Dress Code",
-            dress_code_data: "Semi-formal",
-            video_link: "Video Link",
-            // shuttle bus
-            shuttle_bus: "Transportation",
-            time: "Time",
-            pick_up_location: "Pick Up Location",
-            pick_up_location_data: "Sunny Bay Station exit A",
-        },
-        events: {
-            title: "Upcoming Events",
-            date: "Date",
-            events: [
-                {
-                    title: "Propose",
-                    description: "this is a sample description",
-                    date: "2022-01-22",
-                },
-                {
-                    title: "this is a sample title",
-                    description: "this is a sample description",
-                    date: "2022-01-22",
-                }, {
-                    title: "this is a sample title",
-                    description: "this is a sample description",
-                    date: "2022-01-22",
-                },
-                {
-                    title: "this is a sample title",
-                    description: "this is a sample description",
-                    date: "2022-01-22",
-                }
-            ]
-        },
-        gallery: {
-            gallery: "Gallery",
-            sections: [
-                {
-                    title: "Proposal",
-                    memories: [
-                        // {
-                        //     title: "sample title",
-                        //     description: "if you have something to say about this photos, you can give me a description and title"
-                        // }
-                    ]
-                },
-                {
-                    title: "Pre-Wedding",
-                    memories: [
-
-                    ]
-                },
-                {
-                    title: "Betrothal",
-                    memories: [
-
-                    ]
-                },
-                {
-                    title: "Date",
-                    memories: [
-
-                    ]
-                },
-                {
-                    title: "Graduation",
-                    memories: [
-
-                    ]
-                },
-                {
-                    title: "Our friends",
-                    memories: [
-
-                    ]
-                },
-            ]
-        }
+  en: {
+    layout: {
+      home: "Home",
+      information: "Wedding Day Information",
+      events: "Upcoming Events",
+      story: "Our Story",
+      gallery: "Our Gallery",
     },
-    "zh-hk": {
-        layout: {
-            home: "主頁",
-            information: "詳情",
-            events: "流程",
-            story: "我們的故事",
-            gallery: "相簿",
+    home: {
+      wedding_day: "We are getting married!",
+      story: "Our Story",
+      info: "Information",
+      about_us: "About Us",
+      des1: "We met in church, a place blessed by the Lord, in 2013. Despite our characters being so different and opposite like sense and sensitivity, we draw to each other seeing how beautiful the heart and soul inside us that make the others become better and emerged in love. - Fanny",
+      des2: "After countless journeys and adventures, we finally decided to take the next step in our lives as husband and wife. What other way to celebrate this, than in the company of our beloved family and friends!",
+      day_until: "Date We Getting Married",
+      days: "Days",
+      hours: "Hours",
+      minutes: "Minutes",
+      seconds: "Seconds",
+    },
+    info: {
+      location: "Location",
+      details: "Details",
+      gallery: "Gallery",
+      title: "Wedding Day Information",
+      // details
+      date_time: "Date & Time",
+      date_time_data: "5 March 2022, 13 : 30 - 16 : 30",
+      location_data: "Auberge Discovery Bay Hotel, Hong Kong",
+      dress_code: "Dress Code",
+      dress_code_data: "Semi-formal",
+      video_link: "Video Link",
+      // shuttle bus
+      shuttle_bus: "Transportation",
+      time: "Time",
+      pick_up_location: "Pick Up Location",
+      pick_up_location_data: "Sunny Bay Station exit A",
+    },
+    events: {
+      title: "Upcoming Events",
+      date: "Date",
+      events: [
+        {
+          title: "Propose",
+          description: "this is a sample description",
+          date: "2022-01-22",
         },
-        home: {
-            wedding_day: "婚禮",
-            story: "Our Story",
-            info: "婚禮詳情",
-            about_us: "關於我們",
-            des1: "纖雲弄巧，飛星傳恨，銀漢迢迢暗度。金風玉露一相逢，便勝卻人間無數。柔情似水，佳期如夢，忍顧鵲橋歸路。兩情若是久長時，又豈在朝朝暮暮。纖雲弄巧，飛星傳恨，銀漢迢迢暗度。金風玉露一相逢，便勝卻人間無數。柔情似水，佳期如夢，忍顧鵲橋歸路。兩情若是久長時，又豈在朝朝暮暮。",
-            des2: "纖雲弄巧，飛星傳恨，銀漢迢迢暗度。金風玉露一相逢，便勝卻人間無數。柔情似水，佳期如夢，忍顧鵲橋歸路。兩情若是久長時，又豈在朝朝暮暮。纖雲弄巧，飛星傳恨，銀漢迢迢暗度。金風玉露一相逢，便勝卻人間無數。柔情似水，佳期如夢，忍顧鵲橋歸路。兩情若是久長時，又豈在朝朝暮暮。",
-            day_until: "離我們婚禮還有...",
-            days: "天",
-            hours: "時",
-            minutes: "分",
-            seconds: "秒",
+        {
+          title: "this is a sample title",
+          description: "this is a sample description",
+          date: "2022-01-22",
         },
-        info: {
-            location: "地點",
-            details: "詳情",
-            gallery: "圖片",
-            title: "婚禮詳情",
-            // details 
-            date_time: "日期及時間",
-            date_time_data: "2022 年 3 月 5 日，13 : 30 - 16 : 30",
-            location_data: "香港愉景灣酒店",
-            dress_code: "著裝規範",
-            dress_code_data: "半正式",
-            video_link: "直播片段",
-            // shuttle bus
-            shuttle_bus: "交通",
-            time: "時間",
-            pick_up_location: "接載地點",
-            pick_up_location_data: "欣澳站 A 出口"
+        {
+          title: "this is a sample title",
+          description: "this is a sample description",
+          date: "2022-01-22",
         },
-        events: {
-            title: "流程",
-            date: "日期",
-            events: [
-                {
-                    title: "求婚",
-                    description: "這是例子內容",
-                    date: "2022-01-22",
-                },
-                {
-                    title: "這是例子標題",
-                    description: "這是例子內容",
-                    date: "2022-01-22",
-                },
-                {
-                    title: "這是例子標題",
-                    description: "這是例子內容",
-                    date: "2022-01-22",
-                },
-                {
-                    title: "這是例子標題",
-                    description: "這是例子內容",
-                    date: "2022-01-22",
-                },
-            ],
+        {
+          title: "this is a sample title",
+          description: "this is a sample description",
+          date: "2022-01-22",
         },
-        gallery: {
-            gallery: "相簿",
-            sections: [
-                {
-                    title: "求婚",
-                    memories: [
-                        // {
-                        //     title : "例子標題", 
-                        //     description : "如果你對這張照片有什麼想說的，你可以給我一個描述和標題"
-                        // }
-                    ]
-                }, {
-                    title: "婚紗照",
-                    memories: []
-                }, {
-                    title: "婚紗照",
-                    memories: []
-                }, {
-                    title: "婚紗照",
-                    memories: []
-                }, {
-                    title: "婚紗照",
-                    memories: []
-                }, {
-                    title: "婚紗照",
-                    memories: []
-                }
-            ]
-        }
-    }
-}
+      ],
+    },
+    gallery: {
+      gallery: "Gallery",
+      sections: [
+        {
+          title: "Proposal",
+          memories: [
+            // {
+            //     title: "sample title",
+            //     description: "if you have something to say about this photos, you can give me a description and title"
+            // }
+          ],
+        },
+        {
+          title: "Pre-Wedding",
+          memories: [],
+        },
+        {
+          title: "Betrothal",
+          memories: [],
+        },
+        {
+          title: "Date",
+          memories: [],
+        },
+        {
+          title: "Graduation",
+          memories: [],
+        },
+        {
+          title: "Our friends",
+          memories: [],
+        },
+      ],
+    },
+  },
+  "zh-hk": {
+    layout: {
+      home: "主頁",
+      information: "詳情",
+      events: "流程",
+      story: "我們的故事",
+      gallery: "相簿",
+    },
+    home: {
+      wedding_day: "婚禮",
+      story: "Our Story",
+      info: "婚禮詳情",
+      about_us: "關於我們",
+      des1: "我們於 2013 年在一個蒙主祝福的教堂相遇。儘管我們的性格在理智和敏感方面如此不同和截然相反，但我們彼此吸引，看到對方心靈的美麗，希望與對方一同成長而墮入愛河。",
+      des2: "經過大大小小的冒險和決定，我們終於決定以夫妻的身份邁出人生的下一步。除了在我們心愛的家人和朋友的陪伴舉行婚禮下，還有什麼其他方式來慶祝呢？",
+      day_until: "離我們婚禮還有...",
+      days: "天",
+      hours: "時",
+      minutes: "分",
+      seconds: "秒",
+    },
+    info: {
+      location: "地點",
+      details: "詳情",
+      gallery: "圖片",
+      title: "婚禮詳情",
+      // details
+      date_time: "日期及時間",
+      date_time_data: "2022 年 7 月 30 日，13 : 30 - 16 : 30",
+      location_data: "香港愉景灣酒店",
+      dress_code: "著裝規範",
+      dress_code_data: "半正式",
+      video_link: "直播片段",
+      // shuttle bus
+      shuttle_bus: "交通",
+      time: "時間",
+      pick_up_location: "接載地點",
+      pick_up_location_data: "欣澳站 A 出口",
+    },
+    events: {
+      title: "流程",
+      date: "日期",
+      events: [
+        {
+          title: "求婚",
+          description: "這是例子內容",
+          date: "2022-01-22",
+        },
+        {
+          title: "這是例子標題",
+          description: "這是例子內容",
+          date: "2022-01-22",
+        },
+        {
+          title: "這是例子標題",
+          description: "這是例子內容",
+          date: "2022-01-22",
+        },
+        {
+          title: "這是例子標題",
+          description: "這是例子內容",
+          date: "2022-01-22",
+        },
+      ],
+    },
+    gallery: {
+      gallery: "相簿",
+      sections: [
+        {
+          title: "求婚",
+          memories: [
+            // {
+            //     title : "例子標題",
+            //     description : "如果你對這張照片有什麼想說的，你可以給我一個描述和標題"
+            // }
+          ],
+        },
+        {
+          title: "婚紗照",
+          memories: [],
+        },
+        {
+          title: "過大禮",
+          memories: [],
+        },
+        {
+          title: "約會",
+          memories: [],
+        },
+        {
+          title: "畢業照",
+          memories: [],
+        },
+        {
+          title: "我們的朋友",
+          memories: [],
+        },
+      ],
+    },
+  },
+};
