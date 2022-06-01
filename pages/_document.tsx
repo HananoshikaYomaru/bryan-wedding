f6887b7121fb14d4992bf7402cb54e8df6917543
Hananoshika Yomaru <brenkysbwim@gmail.com>
2022-06-01 14:16:18 +0800

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/wedding/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/wedding/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/wedding/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/wedding/site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
