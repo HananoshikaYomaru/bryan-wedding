72b37359123820239a518d28afa4abdc4fc75b3f
Hananoshika Yomaru <brenkysbwim@gmail.com>
2022-06-01 23:22:01 +0800

    styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,31 @@
-import Image from "next/image"
-import cover from "../public/images/cover.webp"
-import flower from "../public/images/flower.webp"
-
+import moment from "moment";
+import Image from "next/image";
+import cover from "../public/images/cover.webp";
+import flower from "../public/images/flower.webp";
 
 const Footer = () => {
-    return <div className="h-64 w-full relative">
-        <Image src={cover} layout="fill" objectFit="cover" className="absolute pointer-events-none"></Image>
-        <div className="absolute  mt-10 inset-0 left-1/2 w-fit flex flex-col items-center   transform -translate-x-1/2">
-            {/* <div className="flex flex-row">
-                <Image src={flower} width={50} height={50} />
-                <Image src={flower} width={50} height={50} />
-                <Image src={flower} width={50} height={50} />
-            </div> */}
-        <div className="flex flex-col   font-wenyue w-fit items-center text-sky-100 text-xs mt-10 sm:text-sm">
-            <p>Copyright © 2022 YomaruHananoshika. All rights reversed.</p>
-            <a className="text-gray-400 underline" href="mailto:mlkyeung@connect.ust.hk">mlkyeung@connect.ust.hk</a>
-        </div>
-        </div>
-    </div>
-}
+  return (
+    <div className="h-32 sm:h-64 w-full relative">
+      <Image
+        src={cover}
+        layout="fill"
+        objectFit="cover"
+        className="absolute pointer-events-none"
+      ></Image>
 
-export default Footer 
+      <div className="absolute   top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 flex flex-col   font-wenyue w-fit items-center text-sky-100 text-xs  sm:text-sm">
+        <p>
+          Copyright © {moment().year()} YomaruHananoshika. All rights reversed.
+        </p>
+        <a
+          className="text-gray-400 underline"
+          href="mailto:mlkyeung@connect.ust.hk"
+        >
+          mlkyeung@connect.ust.hk
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
